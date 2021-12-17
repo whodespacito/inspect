6c8a3b4f06cae8ac7b6ae292131a4de926b2e937
whodespacito <58792361+whodespacito@users.noreply.github.com>
2021-12-17 12:59:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 let x = 0;
 let y = 0;
 document.addEventListener("mousemove", (event) => {
-  x = event.movementX;
-  y = event.movementY;
+  x = event.pageX;
+  y = event.pageY;
 })
 document.addEventListener("click", (event) => {
   var element = document.elementFromPoint(x, y);
-  alert(`element details: \n 
-  id: ${element.id} \n 
-  class: ${element.className} \n
-  innerHTML: ${element.InnerHTML}
-    `)
+  alert(element)
 })
