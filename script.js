ac969bb45f0712d493d66b2c8ea1119d99ce2606
whodespacito <58792361+whodespacito@users.noreply.github.com>
2021-12-17 13:06:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ document.addEventListener("mousemove", (event) => {
   x = event.pageX;
   y = event.pageY;
 })
-document.addEventListener("click", (event) => {
+document.addEventListener("keydown", (event) => {
+  if (event.key == "\\") && (event.altKey)
   var element = document.elementFromPoint(x, y);
   alert(element)
 })
