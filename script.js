184e345b318c180f78bcaa09980f9be54d4c9e01
whodespacito <58792361+whodespacito@users.noreply.github.com>
2021-12-17 12:08:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ document.addEventListener("mousemove", (event) => {
   x = event.movementX;
   y = event.movementY;
 })
-document.addEventListener("keydown", (event) => {
-  if (event.key == "\\") {
-    var element = document.elementFromPoint(x, y);
-    alert(`element details: \n 
-    id: ${element.id} \n 
-    class: ${element.className} \n
-    innerHTML: ${element.InnerHTML}
+document.addEventListener("click", (event) => {
+  var element = document.elementFromPoint(x, y);
+  alert(`element details: \n 
+  id: ${element.id} \n 
+  class: ${element.className} \n
+  innerHTML: ${element.InnerHTML}
     `)
-  }
 })
