adb3e2cdcfd23cfdf93f7d93767cbf917d0abf16
whodespacito <58792361+whodespacito@users.noreply.github.com>
2021-12-17 13:29:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ document.addEventListener("mousemove", (event) => {
 })
 document.addEventListener("keydown", (event) => {
   if (event.key == "\\" && event.altKey) {
-  var element = document.elementFromPoint(x, y);
-  alert(element)
+    var element = document.elementFromPoint(x, y);
+    var string = "element details:"
+    for (const [key, value] of Object.entries(element)) {
+      string = string + "\n" + key + ": " + value
+    }
+    alert(string)
   }
 })
