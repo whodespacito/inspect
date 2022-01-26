38811f30750fcf035511b1a36fefcd8a90bb38d4
whodespacito <58792361+whodespacito@users.noreply.github.com>
2022-01-26 09:19:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-let x = 0;
-let y = 0;
-document.addEventListener("mousemove", (event) => {
-  x = event.pageX;
-  y = event.pageY;
-})
-document.addEventListener("keydown", (event) => {
-  if (event.key == "\\" && event.altKey) {
-    var element = document.elementFromPoint(x, y);
-    alert(
-    `element details:
-     id: ${element.id}
-     class: ${element.className}
-     tag: ${element.tagName}     
-     innerHTML: ${element.innerHTML}
-    `
-    )
-  }
-})
+window.main = function() {
+  let x = 0;
+  let y = 0;
+  document.addEventListener("mousemove", (event) => {
+    x = event.pageX;
+    y = event.pageY;
+  })
+  document.addEventListener("keydown", (event) => {
+    if (event.key == "\\" && event.altKey) {
+      var element = document.elementFromPoint(x, y);
+      alert(
+      `element details:
+       id: ${element.id}
+       class: ${element.className}
+       tag: ${element.tagName}     
+       innerHTML: ${element.innerHTML}
+      `
+      )
+    }
+  })
+}
+//cause blooket broke alert()
+window.alt = function() {
+  let x = 0;
+  let y = 0;
+  document.addEventListener("mousemove", (event) => {
+    x = event.pageX;
+    y = event.pageY;
+  })
+  document.addEventListener("keydown", (event) => {
+    if (event.key == "\\" && event.altKey) {
+      var element = document.elementFromPoint(x, y);
+      window.open(`https://elementdetails.com/id/${element.id}/class/${element.className}/tag/${element.tagName}/innerHTML/${element.innerHTML}`)
+    }
+  })
+}
+  
