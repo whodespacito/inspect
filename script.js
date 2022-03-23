7b375b41af4e7b3e3edd14c31a67f7f97cd0fd8a
whodespacito <58792361+whodespacito@users.noreply.github.com>
2022-03-23 08:37:51 -0400

    remove alternate
    it was bad who thought of that

--- script.js
+++ script.js
@@ -19,19 +19,5 @@ window.main = function() {
     }
   })
 }
-//cause blooket broke alert()
-window.alt = function() {
-   window.x = 0;
-  window.y = 0;
-  document.addEventListener("mousemove", (event) => {
-    window.x = event.pageX;
-    window.y = event.pageY;
-  })
-  document.addEventListener("keydown", (event) => {
-    if (event.key == "\\" && event.altKey) {
-      var element = document.elementFromPoint(x, y);
-      window.open(`https://elementdetails.com/id/${element.id}/class/${element.className}/tag/${element.tagName}/innerHTML/${element.innerHTML}`)
-    }
-  })
-}
+
   
